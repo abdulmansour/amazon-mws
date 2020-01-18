4ad1c4289e6d3b8550e7dfe1acb7f0eabaa3f37c
abdulmansour <molate1591@gmail.com>
2020-01-18 15:28:24 -0500

    fetch orders into a csv

--- fetch_orders.py
+++ fetch_orders.py
@@ -1,5 +1,6 @@
-import mws, os, pprint
-import pandas as pd 
+import mws, os
+import pandas as pd
+import time
 
 data = []
 
@@ -17,26 +18,38 @@ def main():
     append_address_info(response)
 
     while('NextToken' in response):
-        next_token = response['NextToken']['value']
-        response = orders_api.list_orders_by_next_token(next_token).parsed
-        append_address_info(response)
+        try:
+            next_token = response['NextToken']['value']
+            response = orders_api.list_orders_by_next_token(next_token).parsed
+            append_address_info(response)
 
-    print('Done fetching Orders data...')   
+        except:
+            print("503 Server Error: Service Unavailable to process url. Will try again...")
+            time.sleep(1)
+
+    print('Done fetching Orders data...')
 
     df = pd.DataFrame(data, columns=['OrderId','City','CountryCode','PostalCode','StateOrRegion'])
     print(df)
 
+    file_name = 'orders.csv'
+    print('Writing df into ' + file_name + ' ...')
+    df.to_csv(file_name, encoding='utf-8', index=False)
+    print('Writing complete...')
+
+#append order address info into data list    
 def append_address_info(response):
     orders_list = response['Orders']['Order']
     for o in orders_list:
         if ('ShippingAddress' in o):
             data.append([
-                o['AmazonOrderId']['value'], 
+                o['AmazonOrderId']['value'],
                 o['ShippingAddress']['City']['value'],
                 o['ShippingAddress']['CountryCode']['value'],
                 o['ShippingAddress']['PostalCode']['value'],
                 o['ShippingAddress']['StateOrRegion']['value'],
             ])
+
 
 if __name__ == "__main__":
     main()
